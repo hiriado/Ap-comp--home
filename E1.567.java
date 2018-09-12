45017f8c6c8018b42bc80ea445c4809cfef60e92
Brandon F <42645342+hiriado@users.noreply.github.com>
2018-09-12 19:46:50 -0400

    Chapter 1, Review #2

---
+++ E1.567.java
@@ -0,0 +1,33 @@
+import cs1.Keyboard;
+
+
+public class E1.567
+{
+	public static void main(String[] args)
+	{
+        //5
+        System.out.println("");
+        System.out.println(" ____________");
+        System.out.println("|  Brandon   |");
+        System.out.println("|____________|");
+
+        //6
+        System.out.println("******    ");
+        System.out.println("*	*  	  *	");
+        System.out.println("***     *******		****    ****");
+        System.out.println("*	*		*   *  *   *");
+        System.out.println("*	*	  *	*    **	   *");
+        System.out.println("*	*	  *	*	   *");
+        System.out.println("*	*	  *	*	   *");
+        //7
+        System.out.println("-... .-. .- -. -.. --- -.");
+        
+
+
+       
+
+
+
+    }
+
+}
